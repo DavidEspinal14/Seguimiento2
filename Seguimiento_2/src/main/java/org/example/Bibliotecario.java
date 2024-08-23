b4298d5654763e861b52abe4ef76b3729b1a21b3
DavidEspinal14 <juand.espinalg@uqvirtual.edu.co>
2024-08-23 08:32:47 -0500

    Crear clases y crear atributos de Empleado

---
+++ Seguimiento_2/src/main/java/org/example/Bibliotecario.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Bibliotecario extends Empleado{
+    public Bibliotecario(String nombre, int edad, String cedula){
+        super(nombre, edad, cedula);
+    }
+
+}
